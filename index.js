c9c246908644b92ee8b1bcac2b3bb7bd07a3fd31
Matt Travi <programmer@travi.org>
2020-09-14 00:00:17 -0500

    refactor(base-config): made this the base config
    rather than extending the travi config

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = {extends: ['travi']};
+const config = require('@commitlint/config-conventional');
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-case': [2, 'always', 'kebab-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        ...config.rules['type-enum'][2],
+        'wip'
+      ]
+    ]
+  }
+};
